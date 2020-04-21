97eec97f88d6914ecb7335888177f4c90d38a019
John Doeashishprasad2163 <ashishprasad2163@gmail.com>
2020-04-22 02:10:11 +0530

    bug fixes

--- public/js/main.js
+++ public/js/main.js
@@ -1,67 +1,140 @@
-const chatForm = document.getElementById('chat-form');
-const chatMessages = document.querySelector('.chat-messages');
-const roomName = document.getElementById('room-name');
-const userList = document.getElementById('users');
+let Peer = require('simple-peer');
+let socket = io();
+const video = document.querySelector('video');
+const filter = document.querySelector('#filter');
+const checkboxTheme = document.querySelector('#theme');
+let client = {};
+let currentFilter;
+//get stream
+navigator.mediaDevices
+  .getUserMedia({ video: true, audio: true })
+  .then((stream) => {
+    socket.emit('NewClient');
+    video.srcObject = stream;
+    video.play();
 
-// Get username and room from URL
-const { username, room } = Qs.parse(location.search, {
-  ignoreQueryPrefix: true,
+    filter.addEventListener('change', (event) => {
+      currentFilter = event.target.value;
+      video.style.filter = currentFilter;
+      SendFilter(currentFilter);
+      event.preventDefault;
+    });
+
+    //used to initialize a peer
+    function InitPeer(type) {
+      let peer = new Peer({
+        initiator: type == 'init' ? true : false,
+        stream: stream,
+        trickle: false,
+      });
+      peer.on('stream', function (stream) {
+        CreateVideo(stream);
+      });
+      //This isn't working in chrome; works perfectly in firefox.
+      // peer.on('close', function () {
+      //     document.getElementById("peerVideo").remove();
+      //     peer.destroy()
+      // })
+      peer.on('data', function (data) {
+        let decodedData = new TextDecoder('utf-8').decode(data);
+        let peervideo = document.querySelector('#peerVideo');
+        peervideo.style.filter = decodedData;
+      });
+      return peer;
+    }
+
+    //for peer of type init
+    function MakePeer() {
+      client.gotAnswer = false;
+      let peer = InitPeer('init');
+      peer.on('signal', function (data) {
+        if (!client.gotAnswer) {
+          socket.emit('Offer', data);
+        }
+      });
+      client.peer = peer;
+    }
+
+    //for peer of type not init
+    function FrontAnswer(offer) {
+      let peer = InitPeer('notInit');
+      peer.on('signal', (data) => {
+        socket.emit('Answer', data);
+      });
+      peer.signal(offer);
+      client.peer = peer;
+    }
+
+    function SignalAnswer(answer) {
+      client.gotAnswer = true;
+      let peer = client.peer;
+      peer.signal(answer);
+    }
+
+    function CreateVideo(stream) {
+      CreateDiv();
+
+      let video = document.createElement('video');
+      video.id = 'peerVideo';
+      video.srcObject = stream;
+      video.setAttribute('class', 'embed-responsive-item');
+      document.querySelector('#peerDiv').appendChild(video);
+      video.play();
+      //wait for 1 sec
+      setTimeout(() => SendFilter(currentFilter), 1000);
+
+      video.addEventListener('click', () => {
+        if (video.volume != 0) video.volume = 0;
+        else video.volume = 1;
+      });
+    }
+
+    function SessionActive() {
+      document.write('Session Active. Please come back later');
+    }
+
+    function SendFilter(filter) {
+      if (client.peer) {
+        client.peer.send(filter);
+      }
+    }
+
+    function RemovePeer() {
+      document.getElementById('peerVideo').remove();
+      document.getElementById('muteText').remove();
+      if (client.peer) {
+        client.peer.destroy();
+      }
+    }
+
+    socket.on('BackOffer', FrontAnswer);
+    socket.on('BackAnswer', SignalAnswer);
+    socket.on('SessionActive', SessionActive);
+    socket.on('CreatePeer', MakePeer);
+    socket.on('Disconnect', RemovePeer);
+  })
+  .catch((err) => document.write(err));
+
+checkboxTheme.addEventListener('click', () => {
+  if (checkboxTheme.checked == true) {
+    document.body.style.backgroundColor = '#212529';
+    if (document.querySelector('#muteText')) {
+      document.querySelector('#muteText').style.color = '#fff';
+    }
+  } else {
+    document.body.style.backgroundColor = '#fff';
+    if (document.querySelector('#muteText')) {
+      document.querySelector('#muteText').style.color = '#212529';
+    }
+  }
 });
 
-const socket = io();
-
-// Join chatroom
-socket.emit('joinRoom', { username, room });
-
-// Get room and users
-socket.on('roomUsers', ({ room, users }) => {
-  outputRoomName(room);
-  outputUsers(users);
-});
-
-// Message from server
-socket.on('message', (message) => {
-  console.log(message);
-  outputMessage(message);
-
-  // Scroll down
-  chatMessages.scrollTop = chatMessages.scrollHeight;
-});
-
-// Message submit
-chatForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  // Get message text
-  const msg = e.target.elements.msg.value;
-
-  // Emit message to server
-  socket.emit('chatMessage', msg);
-
-  // Clear input
-  e.target.elements.msg.value = '';
-  e.target.elements.msg.focus();
-});
-
-// Output message to DOM
-function outputMessage(message) {
-  const div = document.createElement('div');
-  div.classList.add('message');
-  div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
-  <p class="text">
-    ${message.text}
-  </p>`;
-  document.querySelector('.chat-messages').appendChild(div);
-}
-
-// Add room name to DOM
-function outputRoomName(room) {
-  roomName.innerText = room;
-}
-
-// Add users to DOM
-function outputUsers(users) {
-  userList.innerHTML = `
-    ${users.map((user) => `<li>${user.username}</li>`).join('')}
-  `;
+function CreateDiv() {
+  let div = document.createElement('div');
+  div.setAttribute('class', 'centered');
+  div.id = 'muteText';
+  div.innerHTML = 'Click to Mute/Unmute';
+  document.querySelector('#peerDiv').appendChild(div);
+  if (checkboxTheme.checked == true)
+    document.querySelector('#muteText').style.color = '#fff';
 }
